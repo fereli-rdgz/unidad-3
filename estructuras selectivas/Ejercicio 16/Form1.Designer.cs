a01cf4daf19af8066ff7524a99f8000696eabd9e
fereli-rdgz <ferelirdgz1826@gmail.com>
2024-11-27 21:22:46 -0600

    Add files via upload

---
+++ estructuras selectivas/Ejercicio 16/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Ejercicio_16
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(32, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(698, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Determine si el triángulo es equilátero isósceles o escaleno";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(88, 72);
+            label2.Name = "label2";
+            label2.Size = new Size(164, 23);
+            label2.TabIndex = 1;
+            label2.Text = "Longitud lado A";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(88, 162);
+            label3.Name = "label3";
+            label3.Size = new Size(161, 23);
+            label3.TabIndex = 2;
+            label3.Text = "Longitud lado B";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(88, 241);
+            label4.Name = "label4";
+            label4.Size = new Size(165, 23);
+            label4.TabIndex = 3;
+            label4.Text = "Longitud lado C";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(299, 173);
+            label5.Name = "label5";
+            label5.Size = new Size(172, 23);
+            label5.TabIndex = 4;
+            label5.Text = "Tipo de triangulo";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(88, 98);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(164, 29);
+            textBox1.TabIndex = 5;
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(88, 188);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(161, 29);
+            textBox2.TabIndex = 6;
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(88, 267);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(165, 29);
+            textBox3.TabIndex = 7;
+            textBox3.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(527, 167);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(229, 29);
+            textBox4.TabIndex = 8;
+            textBox4.TextAlign = HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Olive;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(299, 86);
+            button1.Name = "button1";
+            button1.Size = new Size(128, 41);
+            button1.TabIndex = 9;
+            button1.Text = "Decisión";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.PaleGoldenrod;
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(462, 86);
+            button2.Name = "button2";
+            button2.Size = new Size(128, 41);
+            button2.TabIndex = 10;
+            button2.Text = "Borrar";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.DarkKhaki;
+            button3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.Location = new Point(628, 86);
+            button3.Name = "button3";
+            button3.Size = new Size(128, 41);
+            button3.TabIndex = 11;
+            button3.Text = "Salir";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Cornsilk;
+            ClientSize = new Size(898, 339);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+    }
+}
