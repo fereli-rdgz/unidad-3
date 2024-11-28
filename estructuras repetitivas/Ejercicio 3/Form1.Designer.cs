ac6f958473a2c0ee6d727ad4e69772c7ac45489e
fereli-rdgz <ferelirdgz1826@gmail.com>
2024-11-27 21:17:10 -0600

    Add files via upload

---
+++ estructuras repetitivas/Ejercicio 3/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Ejercicio_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            listBox1 = new ListBox();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Navy;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(21, 140);
+            button1.Name = "button1";
+            button1.Size = new Size(353, 42);
+            button1.TabIndex = 0;
+            button1.Text = "Añadir";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Navy;
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.Location = new Point(21, 272);
+            button2.Name = "button2";
+            button2.Size = new Size(353, 43);
+            button2.TabIndex = 1;
+            button2.Text = "Sumar números";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(63, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(549, 32);
+            label1.TabIndex = 2;
+            label1.Text = "Este programa suma los números capturados";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(21, 88);
+            label2.Name = "label2";
+            label2.Size = new Size(215, 23);
+            label2.TabIndex = 3;
+            label2.Text = "Introduce un número";
+            label2.Click += label2_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = SystemColors.GradientInactiveCaption;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 21;
+            listBox1.Location = new Point(420, 88);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(176, 277);
+            listBox1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(14, 342);
+            label3.Name = "label3";
+            label3.Size = new Size(229, 23);
+            label3.TabIndex = 5;
+            label3.Text = "La suma de todos son:";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.GradientInactiveCaption;
+            textBox1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(249, 88);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 34);
+            textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.GradientActiveCaption;
+            textBox2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(249, 331);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 34);
+            textBox2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Lavender;
+            ClientSize = new Size(676, 416);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(listBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private Label label2;
+        private ListBox listBox1;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+    }
+}
---
+++ estructuras repetitivas/Ejercicio 4/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Ejercicio_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            listBox1 = new ListBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.DarkGreen;
+            button1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(111, 396);
+            button1.Name = "button1";
+            button1.Size = new Size(263, 42);
+            button1.TabIndex = 0;
+            button1.Text = "Imprime en pantalla";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = Color.Honeydew;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 21;
+            listBox1.Location = new Point(111, 59);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(263, 319);
+            listBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(455, 31);
+            label1.TabIndex = 2;
+            label1.Text = "Genera los números del 1 al 20 con While";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.PaleGreen;
+            ClientSize = new Size(482, 469);
+            Controls.Add(label1);
+            Controls.Add(listBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private ListBox listBox1;
+        private Label label1;
+    }
+}
